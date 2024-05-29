86e9247784652892f90ae696ba3a5d3f9eb61a79
Prasanth <prasanthmurugesan549@gmail.com>
2024-05-29 12:45:46 +0530

    level 1
    Task2 and Task 3

---
+++ script landing.js
@@ -0,0 +1,11 @@
+
+const heroButton = document.querySelector('#hero button');
+const pricingButton = document.querySelector('#pricing button');
+
+heroButton.addEventListener('click', () => {
+    console.log('Get Started button clicked!');
+});
+
+pricingButton.addEventListener('click', () => {
+    console.log('Sign Up button clicked!');
+});
